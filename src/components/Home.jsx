dabf098fe613b43495e4722c9d75dee556188627
Aman Kumar Routh <routhaman831@gmail.com>
2024-10-18 21:23:23 +0530

    font edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,27 +5,27 @@ import garage from '../../public/assets/garage.jpg'
 
 const Home = () => {
   return (
-    <div className=' w-full'>
+    <div className='w-full'>
       
         <div className='flex flex-col md:flex-row justify-around my-10 px-4 md:px-0'>
-            <div className="h-[40vh] md:h-[40vh] w-full md:w-[28vw]  rounded-xl shadow-md shadow-black mb-4 md:mb-0 hover:scale-105 hover:-translate-y-2 transition-all">
+            <div className="h-[40vh] md:h-[40vh] w-full md:w-[28vw] rounded-xl shadow-md shadow-black mb-4 md:mb-0 hover:scale-105 hover:-translate-y-2 transition-all">
                 <img src={house} className='w-full md:w-[28vw] h-[35vh] rounded-t-xl' alt="House" />
-                <h1 className="text-center text-black font-bold text-xl mt-2">House For Rent</h1>
+                <h1 className="text-center text-gray-800 font-bold text-xl italic relative top-[-5px] mt-2">House For Rent</h1>
             </div>
             <div className="h-[40vh] w-full md:w-[28vw] rounded-xl mb-4 md:mb-0 shadow-md shadow-black hover:scale-105 hover:-translate-y-2 transition-all">
             <img src={flat} className='w-full md:w-[28vw] h-[35vh] rounded-t-xl' alt="Flat" />
-            <h1 className="text-center text-black font-bold text-xl mt-2">Flat For Rent</h1>
+            <h1 className="text-center text-gray-800 font-bold text-xl italic relative top-[-5px] mt-2">Flat For Rent</h1>
             </div>
         </div>
 
         <div className='flex flex-col md:flex-row justify-around my-10 px-4 md:px-0'>
             <div className="h-[40vh] w-full md:w-[28vw] rounded-xl mb-4 md:mb-0 shadow-md shadow-black hover:scale-105 hover:-translate-y-2 transition-all">
             <img src={shop} className='w-full md:w-[28vw] h-[35vh] rounded-t-xl' alt="Shop" />
-            <h1 className="text-center text-black font-bold text-xl mt-2">Shop For Rent</h1>
+            <h1 className="text-center text-gray-800 font-bold text-xl italic relative top-[-5px] mt-2">Shop For Rent</h1>
             </div>
             <div className="h-[40vh] w-full md:w-[28vw] rounded-xl shadow-md shadow-black hover:scale-105 hover:-translate-y-2 transition-all">
             <img src={garage} className='w-full md:w-[28vw] h-[35vh] rounded-t-xl' alt="Garage" />
-            <h1 className="text-center text-black font-bold text-xl mt-2">Garage For Rent</h1>
+            <h1 className="text-center text-gray-800 font-bold text-xl italic relative top-[-5px] mt-2">Garage For Rent</h1>
             </div>
         </div>
     </div>
