863eedd966a24d04e36011988b8458239f6b1ba0
Prince K <princejaj67@gmail.com>
2024-11-04 10:28:21 +0530

    Fixed banner issue

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -14,19 +14,19 @@ const Banners = () => {
     const set = () =>
       setTimeout(() => {
         setN((n + 1) % bannerList.length);
-      }, 2000);
+      }, 4000);
     set();
     return set;
   });
 
   return (
     <div className="relative h-[50vh] md:h-[35vh] lg:h-[28vh]">
-      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-[50%] md:h-[30%] lg:h-[25%] my-3 mx-auto w-[90%] md:w-[70%] lg:w-1/2 flex justify-center items-center">
-        <img  src={bannerList[n]}  alt="b1"  className=" my-4 bg-red-300 rounded-2xl mx-auto bg-contain max-h-1/3 w-[90%] md:w-[65%] lg:w-[50%] transition-all"/>
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-[50%] md:h-[30%] lg:h-[25%] my-3 mx-auto w-[90%] md:w-[70%] lg:w-1/2 flex justify-center items-center ">
+        <img  src={bannerList[n]}  alt="b1" className=" my-4 bg-red-300 rounded-2xl mx-auto bg-contain max-h-1/3 w-[90%] md:w-[65%] lg:w-[50%] transition-all"/>
       </div>
-      <div className="absolute flex gap-2 m-6 left-1/3 bottom-2">
-        {bannerList.map((item, index) => {
-          return <div key={index} onClick={()=>setN(index)} className="cursor-pointer h-4 w-4 bg-blue-300 rounded-full"></div>;
+      <div className="absolute flex gap-2 mb-6 left-1/2 -translate-x-1/2 bottom-2 items-center">
+        {bannerList.map((i, index) => {
+          return <div key={index} onClick={()=>setN(index)} className={`transition-all cursor-pointer rounded-full ${n===index ? 'bg-red-400 h-4 w-4' : 'bg-blue-100 h-3 w-3'}`}></div>;
         })}
       </div>
     </div>
