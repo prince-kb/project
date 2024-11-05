5f168894a81ee8628ab80cb32b9bfddb5163c371
Prince K <princejaj67@gmail.com>
2024-11-05 16:54:10 +0530

    Improved front page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import house from '../assets/house/house10.jpg';
 import flat from '../assets/flate/flat.jpg';
 import shop from '../assets/shop/Store.jpg';
-import garage from '../assets/garage/garage.jpg';
+import garage from '../assets/garage/garage1.jpg';
 import Banners from './Banners';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,12 +12,8 @@ const Home = () => {
   return (
     <div className='w-full'>
       <Banners />
-
-      <h1 className='text-2xl text-center font-semibold lg:text-3xl mt-6 md:mt-8 lg:mt-10 text-amber-800'>
-        Browse Popular Categories at Homie
-      </h1>
       
-      <div className='container mx-auto px-4 py-8'>
+      <div className='container mx-auto px-4 py-8 mt-4'>
         <h2 className='text-xl font-bold mb-4 text-center'>Search Listings</h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           {/* City Input */}
@@ -36,7 +32,7 @@ const Home = () => {
           </div>
           {/* Property Type Selection */}
           <div>
-            <label htmlFor="propertyType" className='block text-gray-700 mb-2'>PropertyType</label>
+            <label htmlFor="propertyType" className='block text-gray-700 mb-2'>Type</label>
             <select id="propertyType" className='border rounded w-full py-2 px-3'>
               <option value="" disabled selected>Select type</option>
               <option value="house">House</option>
@@ -66,6 +62,10 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <h1 className='text-2xl text-center font-semibold lg:text-3xl mt-6 md:mt-8 lg:mt-10 text-amber-800'>
+        Browse Popular Categories at Homie
+      </h1>
 
       <div className='flex flex-col md:flex-row justify-around my-10 px-4 md:px-0'>
         <div onClick={() => navigate('/salehouse')} className="h-auto w-full md:w-auto rounded-xl mb-4 md:mb-0 transition-all duration-200 ease-out hover:scale-105 hover:-translate-y-2 cursor-pointer border">
